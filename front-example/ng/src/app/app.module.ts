90234eeaf95a09acb720aefe85d82f2e8f685a7c
kentana <kentana.jp@gmail.com>
2018-04-01 00:52:00 +0900

    ng: generate todo component.

--- front-example/ng/src/app/app.module.ts
+++ front-example/ng/src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 import { AppComponent } from './app.component';
+import { TodosComponent } from './todos/todos.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
