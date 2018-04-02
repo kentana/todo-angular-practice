6c12503f7c07a2f7614f6353fe4d62e3478cf390
kentana <kentana.jp@gmail.com>
2018-04-02 12:37:03 +0900

    ng: modify NgbModule import.

--- front-example/ng/src/app/app.module.ts
+++ front-example/ng/src/app/app.module.ts
@@ -16,7 +16,7 @@ import { HttpClientModule } from '@angular/common/http';
   ],
   imports: [
     BrowserModule,
-    NgbModule,
+    NgbModule.forRoot(),
     HttpClientModule,
   ],
   providers: [
