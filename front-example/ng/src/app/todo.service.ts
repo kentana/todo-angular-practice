e5f4a559f445e565bb5af64918d61d8d9fea4e89
kentana <kentana.jp@gmail.com>
2018-04-02 15:20:57 +0900

    ng: add star/done methods to service.

--- front-example/ng/src/app/todo.service.ts
+++ front-example/ng/src/app/todo.service.ts
@@ -24,7 +24,7 @@ export class TodoService {
   getTodoes(): Observable<Todo[]> {
     return this.http.get<Todo[]>(this.todosUrl)
       .pipe(
-        tap(todos => this.log(`fetched todos`)),
+        tap(res => this.log(`fetched todos`)),
         catchError(this.handleError('getTodoes', []))
       );
   }
@@ -33,7 +33,7 @@ export class TodoService {
   getTodo(id: number): Observable<Todo> {
     const url = `${this.todosUrl}/${id}`;
     return this.http.get<Todo>(url).pipe(
-      tap(todos => this.log(`fetched todos`)),
+      tap(res => this.log(`fetched todo`)),
       catchError(this.handleError<Todo>(`getTodo id=${id}`))
     );
   }
@@ -41,7 +41,7 @@ export class TodoService {
   //  add new todo.
   addTodo(todo: Todo): Observable<Todo> {
     return this.http.post<Todo>(this.todosUrl, todo, httpOptions).pipe(
-      tap(todos => this.log(`fetched todos`)),
+      tap(res => this.log(`added todo`)),
       catchError(this.handleError<Todo>('addTodo'))
     );
   }
@@ -52,7 +52,7 @@ export class TodoService {
     const url = `${this.todosUrl}/${id}`;
 
     return this.http.delete<Todo>(url, httpOptions).pipe(
-      tap(todos => this.log(`fetched todos`)),
+      tap(res => this.log(`deleted todo`)),
       catchError(this.handleError<Todo>('deleteTodo'))
     );
   }
@@ -60,13 +60,42 @@ export class TodoService {
   // update todo.
   updateTodo(todo: Todo): Observable<any> {
     return this.http.put(this.todosUrl, todo, httpOptions).pipe(
-      tap(todos => this.log(`fetched todos`)),
+      tap(res => this.log(`updated todo`)),
       catchError(this.handleError<any>('updateTodo'))
     );
   }
 
   // star / unstar
+  starTodo(todo: Todo): Observable<any> {
+    const url = `${this.todosUrl}/star`;
+    return this.http.put(url, {}, httpOptions).pipe(
+      tap(res => this.log(`starred todo`)),
+      catchError(this.handleError<any>('starTodo'))
+    );
+  }
+  unstarTodo(todo: Todo): Observable<any> {
+    const url = `${this.todosUrl}/unstar`;
+    return this.http.put(url, {}, httpOptions).pipe(
+      tap(res => this.log(`unstarred todo`)),
+      catchError(this.handleError<any>('unstarTodo'))
+    );
+  }
+
   // done / undone
+  doneTodo(todo: Todo): Observable<any> {
+    const url = `${this.todosUrl}/done`;
+    return this.http.put(url, {}, httpOptions).pipe(
+      tap(res => this.log(`done todo`)),
+      catchError(this.handleError<any>('doneTodo'))
+    );
+  }
+  undoneTodo(todo: Todo): Observable<any> {
+    const url = `${this.todosUrl}/undone`;
+    return this.http.put(url, {}, httpOptions).pipe(
+      tap(res => this.log(`undone todo`)),
+      catchError(this.handleError<any>('undoneTodo'))
+    );
+  }
 
   /**
    * common error handler.
